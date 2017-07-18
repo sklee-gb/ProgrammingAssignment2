f078e695f3d1ef5b7bbc128ad6b4776b97fdcf97
sklee-gb <attractsky@hanmail.net>
2017-07-18 14:04:08 -0600

    Programming Assignment 2
    Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,14 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+# The function, makeVector creates a matrix containing a function to
+#   1. set the value of the matrix
+#   2. get the value of the matrix
+#   3. set the value of the inverse
+#   4. get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()){
+    ## First  
     inv <- NULL
     set <- function(y){
         x <<- y
@@ -18,7 +23,11 @@ makeCacheMatrix <- function(x = matrix()){
 }
 
 
-## Write a short comment describing this function
+# The following function calculates the inverse of the matrix created 
+# with the above function. However, it first checks to see if the inverse has
+# already been calculated. If so, it gets the inverse from the cache and skips the 
+# computation. Otherwise, it calculates the inverse of the data and sets the value
+# of the inverse in the cache via the setinv function
 
 cacheSolve <- function(x, ...) {
     inv <- x$getinv()
